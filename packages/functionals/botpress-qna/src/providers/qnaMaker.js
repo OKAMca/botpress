6f753813483fce0544e91ec6b8a25f594908f546
Marie-Maxime Tanguay <marie-maxime@okam.ca>
2018-09-14 12:07:21 -0400

    fix: return the anwser with the highest score in bp qna with qna-maker

--- packages/functionals/botpress-qna/src/providers/qnaMaker.js
+++ packages/functionals/botpress-qna/src/providers/qnaMaker.js
@@ -148,7 +148,9 @@ export default class Storage {
       { baseURL: this.knowledgebase.hostName, headers: { Authorization: `EndpointKey ${this.endpointKey}` } }
     )
 
-    return _.orderBy(answers, ['confidence'], ['desc']).map(answer => ({
+    console.log('!!ANWSERS!!', answers)
+
+    return _.orderBy(answers, ['confidence'], ['asc']).map(answer => ({
       ..._.pick(answer, ['questions', 'answer', 'id']),
       confidence: answer.score / 100,
       ...qnaItemData(answer)
